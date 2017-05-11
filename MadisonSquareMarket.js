f40f40b48de809311dee4c24cb05d01c68bca5e1
mlama007 <marialamardo@outlook.com>
2017-05-11 14:48:09 -0400

    finished

---
+++ MadisonSquareMarket.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.cart h3').click(function(){
+    $('.account .dropdown-menu').hide();
+    $('.help .dropdown-menu').hide();
+		$('.cart h3').next().toggle();
+	});
+  $('.account h3').click(function(){
+    $('.cart .dropdown-menu').hide();
+    $('.help .dropdown-menu').hide();
+		$('.account h3').next().toggle();
+	});
+  $('.help h3').click(function(){
+    $('.cart .dropdown-menu').hide();
+    $('.account .dropdown-menu').hide();
+		$('.help h3').next().toggle();  	
+	});
+});
